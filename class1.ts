1cc3fef6b20d32b162991abc8cc716aa4ef18e4b
asp <asp@aptech.com>
2024-04-30 19:30:04 +0500

    hello sir

---
+++ class1.ts
@@ -0,0 +1,6 @@
+var fname="ismail";
+var age=17;
+var salary=100000;
+console.log(fname);
+console.log(age);
+console.log(salary);
---
+++ class2.js
@@ -0,0 +1,30 @@
+// function  add(a:number, b:number)
+// {
+// return(a+b);
+// }
+// console.log(add(1,5));
+// function  sub(a:number, b:number)
+// {
+// return(a-b);
+// }
+// console.log(sub(1,5));
+// function  divide(a:number, b:number)
+// {
+// return(a/b);
+// }
+// console.log(divide(1,5));
+// function  multiply(a:number, b:number)
+// {
+// return(a*b);
+// }
+// console.log(multiply(1,5));
+for (var i = 1; i <= 10; i++) {
+    var number = 5;
+    var result = i * number;
+    console.log(result);
+}
+for (var i = 1; i <= 50; i++) {
+    var number = 2;
+    var result = i * number;
+    console.log(result);
+}
